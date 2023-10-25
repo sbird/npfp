03cf2b71165825046160d0cbfca40f394451f06e
Pooyan Goodarzi <pgood007@ucr.edu>
2023-10-25 09:19:21 -0700

    fixed another typo

--- kdk.py
+++ kdk.py
@@ -5,19 +5,19 @@ import numpy as np
 
 def kick(particle_data, dt):
     """
-        Input is a single particle dictionary. expects the keys 'position', 'velocity', and 'mass'.
-        Kicks the particles velocity one step forward. 
+        Input is a single particle dictionary. expects the keys 'position', 'momentum', and 'mass'.
+        Kicks the particles momentum one step forward. 
         K(dt/2) = (x_t, v_t) --> (x_t, v_t+1) 
       
       """
     acc = gforce(particle_data["id"])/particle_data["mass"]
-    particle_data["velocity"] += acc*dt
+    particle_data["momentum"] += acc*dt
     return particle_data
 
 
 def drift(paticle_data, dt):
     """
-        Input is a single particle dictionary. expects the keys 'position', 'velocity', and 'mass'.
+        Input is a single particle dictionary. expects the keys 'position', 'momentum', and 'mass'.
         Drifts the particles position one step forward. 
         K(dt/2) = (x_t, v_t) --> (x_t, v_t+1) 
       
@@ -28,7 +28,7 @@ def drift(paticle_data, dt):
 
 def kdk(particle_data, dt):
     """
-        Input is a single particle dictionary. expects the keys 'position', 'velocity', and 'mass'.
+        Input is a single particle dictionary. expects the keys 'position', 'momentum', and 'mass'.
         Drifts and kicks the particles position one step forward. 
         (x_t, v_t) --> (x_t+1, v_t+1)
 
