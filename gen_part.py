3ad7b5c9cf9e60da2aeb2915e32184e85681e27e
Pooyan Goodarzi <pgood007@ucr.edu>
2023-11-05 16:23:56 -0800

    generated (partially) initial conditions based on Plummer's model.

--- gen_part.py
+++ gen_part.py
@@ -26,3 +26,35 @@ def generate_particle_info(N):
         particle_data[particle_id] = particle_info
 
     return particle_data
+
+
+def gen_particle_plummer(N):
+    X1 = np.random.uniform(size=N)
+    r = np.power(np.power(X1, -2/3)-1, -0.5)
+    
+    X2 = np.random.uniform(size=N)
+    X3 = np.random.uniform(size=N)
+    z = (1-2*X2)*r
+    x = np.power(r**2 - z**2, 1/2) * np.cos(2*np.pi*X3)
+    y = (r**2 - z**2)**(0.5) * np.sin(2*np.pi*X3)
+
+    velocities = np.array([])
+    i = 0
+    while len(velocities)<N:
+
+        X4 = np.random.uniform()
+        X5 = np.random.uniform()
+        if 0.1*X5 < (X4**2 * (1 - X4**2))**(7/2):    
+            X6 = np.random.uniform()
+            X7 = np.random.uniform()
+            V = X4 * 2**(1/2) * (1 + r[i]**2)**(-1/4)
+            w = (1-2*X6) * V
+            u = (V**2 - w**2)**(1/2) * np.cos(2 * np.pi * X7)
+            v = (V**2 - w**2)**(1/2) * np.sin(2 * np.pi * X7)
+            velocities = np.append(velocities, np.array([u, v, w]))
+            i +=1 
+    
+    print(velocities)
+
+
+gen_particle_plummer(3)
