f744686d108290b2c1951d353668a7a08a14ccf2
Simeon Bird <sbird@ucr.edu>
2023-11-08 10:09:57 -0800

    Basic example on using classes

---
+++ example_classes.py
@@ -0,0 +1,29 @@
+data = {x, y, string}
+
+record_book = {
+author = ""
+published_year = int
+number of times borrowed = int
+ISBN-13 = string
+}
+
+list_of_records = [book1, book2, book3, etc]
+
+list_of_records[0]["ISBN-13"]
+
+class Book:
+    self.author = ""
+    self.published_year = 0
+    self.number of times borrowed = int
+    self.ISBN-13 = string
+
+    def borrow_a_book(self):
+        self.number of times borrowed += 1
+
+
+book = Book()
+book.borrow_a_book()
+
+book.number of times borrowed += 1
+
+book.author
