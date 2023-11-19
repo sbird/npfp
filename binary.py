bbc5d578c0ac86797486cd596b869b672faf2d3e
NiloofarSharei <niloo.sharei@gmail.com>
2023-11-19 11:44:23 -0800

    started binary.py

---
+++ binary.py
@@ -0,0 +1,8 @@
+def spatial(particle1,particle2,mean):
+    r = particle2["position"] - particle1["position"]  
+    if r < mean/100
+        return True
+    
+def grav_bound(particle1,particle2,b):
+    if b< sqrt((np.pi)*(85*np.pi/3)**(2/7)* (Rs**2)*()**
+    
